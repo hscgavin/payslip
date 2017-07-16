2ac760e619af45c737d8f25dc9014b0ea5e783b0
Gavin He <gavin.he@kogan.com.au>
2017-07-16 21:21:54 +1000

    Add taxRateTable for year 2013

---
+++ app/taxRateTable.js
@@ -0,0 +1,21 @@
+// 0 - $18,200     Nil
+// $18,201 - $37,000       19c for each $1 over $18,200
+// $37,001 - $80,000       $3,572 plus 32.5c for each $1 over $37,000
+// $80,001 - $180,000      $17,547 plus 37c for each $1 over $80,000
+// $180,001 and over       $54,547 plus 45c for each $1 over $180,000
+
+
+const taxRateTable = {
+  "2018": [
+
+  ],
+  "2013": [
+    {max: 18200, min: 0, fixed: 0, rate: 0},
+    {max: 37000, min: 18200, fixed: 0, rate: 0.19},
+    {max: 80000, min: 37000, fixed: 3572, rate: 0.325},
+    {max: 180000, min: 80000, fixed: 17547, rate: 0.37},
+    {max: 9999999999, min: 180000, fixed: 54547, rate: 0.45}
+  ]
+}
+
+module.exports = taxRateTable
