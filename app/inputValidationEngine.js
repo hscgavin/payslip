d4c46a5c9b9040575df30ee28a81cee1e369cc5f
Gavin He <gavin.he@kogan.com.au>
2017-07-17 00:46:58 +1000

    Fix bugs in inpuValidationEngine  - Fix salary validation  - percentage robust checking  - Start date with YYYY-MM-DD format

--- app/inputValidationEngine.js
+++ app/inputValidationEngine.js
@@ -27,29 +27,35 @@ inputValidationEngine.checkName = function (name, callback) {
 
 // make sure salary is positve integer or o
 inputValidationEngine.checkSalary = function (salary, callback) {
-  const s = Math.floor(Number(salary))
+  const s = parseInt(salary, 10)
   let errorMsg = null
-  if (String(s) === salary && s >= DEFAULTSCHEMA.minsalary) {
-    errorMsg = `salary must be greater or equal ${DEFAULTSCHEMA.minsalary} and should be integer`
+  if (s != salary || s < DEFAULTSCHEMA.minsalary) {
+    errorMsg = `salary must be greater than or equal to ${DEFAULTSCHEMA.minsalary} and should be integer`
   }
+  callback(errorMsg)
 }
 
 inputValidationEngine.checkSuperRate = function (superRate, callback) {
   let errorMsg = null
-  const convertedSuperRate = parseFloat(superRate)
-  if (!isNaN(convertedSuperRate)) {
-    if (convertedSuperRate >=DEFAULTSCHEMA.minsuperrate && convertedSuperRate <= DEFAULTSCHEMA.minsuperrate) {
-      return callback(errorMsg)
+  if (superRate.indexOf('%') !== -1) {
+    const arr = superRate.split('%')
+    if (arr.length === 2 && !arr[1]) {
+      const convertedSuperRate = parseFloat(arr[0])
+      if (!isNaN(convertedSuperRate)) {
+        if (convertedSuperRate >= DEFAULTSCHEMA.minsuperrate && convertedSuperRate <= DEFAULTSCHEMA.maxsuperrate) {
+          return callback(errorMsg)
+        }
+      }
     }
   }
-  errorMsg = `Super Rate should within ${DEFAULTSCHEMA.minsuperrate} and ${DEFAULTSCHEMA.maxsuperrate}%`
+  errorMsg = `Super Rate should be within ${DEFAULTSCHEMA.minsuperrate} and ${DEFAULTSCHEMA.maxsuperrate}% (e.g. 9%)`
   callback(errorMsg)
 }
 
 inputValidationEngine.checkStartDate = function (date, callback) {
   let errorMsg = null
-  if (!moment(date).isValid()) {
-    errorMsg = 'Please provide a correct date format'
+  if (!moment(date, ['YYYY-MM-DD'], true).isValid()) {
+    errorMsg = 'Please provide a correct date with format YYYY-MM-DD'
   }
   callback(errorMsg)
 }
