88fb52f599fe127cec09a4180850d5a8567d72fc
Gavin He <gavin.he@kogan.com.au>
2017-07-17 00:49:38 +1000

    Create constant taxRateTable which defines ATO tax rates

--- app/taxRateTable.js
+++ app/taxRateTable.js
@@ -7,7 +7,18 @@
 
 const taxRateTable = {
   "2018": [
-
+    {max: 18200, min: 0, fixed: 0, rate: 0},
+    {max: 37000, min: 18200, fixed: 0, rate: 0.19},
+    {max: 87000, min: 37000, fixed: 3572, rate: 0.325},
+    {max: 180000, min: 87000, fixed: 19822, rate: 0.37},
+    {max: 9999999999, min: 180000, fixed: 54232, rate: 0.45}
+  ],
+  "2017": [
+    {max: 18200, min: 0, fixed: 0, rate: 0},
+    {max: 37000, min: 18200, fixed: 0, rate: 0.19},
+    {max: 87000, min: 37000, fixed: 3572, rate: 0.325},
+    {max: 180000, min: 87000, fixed: 19822, rate: 0.37},
+    {max: 9999999999, min: 180000, fixed: 54232, rate: 0.45}
   ],
   "2013": [
     {max: 18200, min: 0, fixed: 0, rate: 0},
